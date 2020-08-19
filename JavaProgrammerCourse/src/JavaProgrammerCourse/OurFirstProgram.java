02784621d6c20b2e1e41cce01cec18a0a17a7198
ELIAMAR GOMEZ <ELIAMAR GOMEZ@DESKTOP-U9841QD>
2020-08-19 23:04:02 +0200

    Primer Commit 9 de Julio:
    Elaborado por Eliamar Gomez Torres, Ingeniero en Tecnologías de la Información

--- JavaProgrammerCourse/src/JavaProgrammerCourse/OurFirstProgram.java
+++ JavaProgrammerCourse/src/JavaProgrammerCourse/OurFirstProgram.java
@@ -1,3 +1,4 @@
+
 package JavaProgrammerCourse;
 
 /**
@@ -5,8 +6,14 @@ package JavaProgrammerCourse;
  * @author ELIAMAR GOMEZ
  */
 public class OurFirstProgram {
+
     //CURSO INICIADO EL DIA MIERCOLES 19 DE AGOSTO DEL 2020.
     //INGENIERIA EN TECNOLOGIAS DE LA INFORMACION.
     
+    public static void main(String[] args) {
+        
+        System.out.println("HELLO WORLD. ");
+        System.out.println("HOW ARE YOU ? ");
+    }
     
 }
